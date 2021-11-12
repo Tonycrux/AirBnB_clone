ef53d8a8886a93e27256eac07c343ca2de32fabc
Timalex1 <timilehinvincent@gmail.com>
2021-11-12 03:24:14 -0800

    Updating console app

--- console.py
+++ console.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """ Main class for AirBNB Console """
-from cmd import Cmd
+import cmd
 
-class HBNBCommand(Cmd):
+class HBNBCommand(cmd.Cmd):
     """ Console that emulate AirBNB """
     prompt = '(hbnb) '
 
-    
+    def do_quit(self, line):
+        """ Quit command to exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ EOF execution to exit the program """
+        return True
+
+    def emptyline(self):
+        """ Empty file """
+        pass
+        
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
