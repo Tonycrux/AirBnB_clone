7f2f949d111a5d41bbfdce4d0a84c4a732fc0088
Tonycrux <sunnexbanty@gmail.com>
2021-11-11 23:11:34 -0800

    init and basemodel update

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,17 @@
 """ Init File Models """
+from models.base_model import BaseModel
+
+
+class Classes(dict):
+    """classes"""
+
+    def __getitem__(self, key):
+        """get item"""
+        try:
+            return super(Classes, self).__getitem__(key)
+        except Exception as e:
+            raise Exception("** class doesn't exist **")
+
+
+models = [BaseModel]
+classes = Classes(**{x.__name__: x for x in models})
