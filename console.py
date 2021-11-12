e374ea3bb1f9fa2b99870b78bc4bebbf1cfda8d3
Timalex1 <timilehinvincent@gmail.com>
2021-11-12 04:01:47 -0800

    Adding necessary files

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ Main class for AirBNB Console """
 import cmd
+from models import storage
+from models.engine import file_storage
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """ Console that emulate AirBNB """
